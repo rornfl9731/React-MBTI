cc1701b6259dfb555953afe7e1b4722323f6c0de
kim <inx0930@gmail.com>
2022-04-25 16:01:43 +0900

    test로직

--- src/pages/Question.js
+++ src/pages/Question.js
@@ -1,7 +1,72 @@
 import React from "react";
+import styled from "styled-components";
+import {ProgressBar,Button} from "react-bootstrap";
+import {QuestionData} from "../assets/data/questiondata"
+import { createSearchParams, useNavigate } from 'react-router-dom';
 
 const Question = () => {
-    return (<div>질문화면</div>)
+    const [questionNo,setQuestionNo] = React.useState(0);
+    const [totalScore,setTotalScore] = React.useState([
+        {id:"EI",score:0},
+        {id:"SN",score:0},
+        {id:"TF",score:0},
+        {id:"JP",score:0}
+    ]);
+    const navigate = useNavigate();
+
+    console.log('totalScore',totalScore);
+
+    const handleClickButtonA = (no,type) =>{
+        const newScore = totalScore.map((s)=>
+            s.id === type ? {id:s.id,score:s.score+no} : s
+        )
+
+        setTotalScore(newScore);
+        if(QuestionData.length !== questionNo+1){
+            setQuestionNo(questionNo+1);
+        }else {
+            const mbti = "ISFP"
+            navigate({
+                pathname: "/result",
+                search: `?${createSearchParams({
+                    mbti: mbti,
+                })}`
+            });
+        }
+
+    }
+
+
+    return (
+        <Wrapper>
+            <ProgressBar striped variant="danger" now={(questionNo / QuestionData.length)*100} style={{marginTop:'20px'}}/>
+            <Title>{QuestionData[questionNo].title}</Title>
+            <ButtonGroup>
+                <Button onClick={()=>handleClickButtonA(1,QuestionData[questionNo].type)} style={{width:'40%',minHeight:'200px',fontSize:'15pt'}}>{QuestionData[questionNo].answera}</Button>
+                <Button onClick={()=>handleClickButtonA(0,QuestionData[questionNo].type)} style={{width:'40%',minHeight:'200px',fontSize:'15pt',marginLeft:'20px'}}>{QuestionData[questionNo].answerb}</Button>
+            </ButtonGroup>
+        </Wrapper>
+    )
 }
 
 export default Question;
+
+
+const Wrapper = styled.div`
+    width: 100%;
+    height: 100vh;
+`;
+
+const Title = styled.h1`
+    font-size: 30pt;
+    font-family:"Rf";
+    text-align: center;
+`;
+
+const ButtonGroup = styled.div`
+    display: flex;
+    justify-content: center;
+    flex-direction: row;
+    margin-top: 20px;
+    font-family: "Rf"; 
+`;
--- src/pages/Result.js
+++ src/pages/Result.js
@@ -1,7 +1,69 @@
 import React from "react";
+import styled from "styled-components";
+import PangImage from "../assets/po_jw.jpg";
+import Button from 'react-bootstrap/Button';
+import {useNavigate} from 'react-router-dom';
 
-const Result = () => {
-    return (<div>결과화면</div>)
+const Result = () =>{
+
+    const navigate = useNavigate();
+    const handleClickButton = () =>{
+        navigate('/question');
+    }
+
+
+
+    return (
+        <Wrapper>
+            <Header>예비집사 판별기</Header>
+            <Contents>
+                <Title>결과 보기</Title>
+                <LogoImage>
+                    <img alt="고양이 사진" src={PangImage} className="rounded-circle" width={350} height={350} />
+                </LogoImage>
+                <Desc>예비 집사님과 찰떡 궁합인 고양이는 땡땡 입니다</Desc>
+                <Button style={{ fontFamily: "Rf" }} onClick={handleClickButton} >
+                    테스트 다시하기
+                </Button>
+            </Contents>
+        </Wrapper>
+    )
 }
 
 export default Result;
+
+const Wrapper = styled.div`
+    width: 100%;
+    height: 100vh;
+`;
+
+const Header = styled.div`
+  font-size: 40pt;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: 'Rf';
+`;
+
+const Title = styled.div`
+  font-size: 30pt;
+  margin-top: 40px;
+  font-family: 'Rf';
+`
+const LogoImage = styled.div`
+  margin-top: 10px;
+  
+`
+const Desc = styled.div`
+  font-size: 20pt;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  font-family: 'Rf';
+`
+const Contents = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  font-family: 'Rf';
+`
